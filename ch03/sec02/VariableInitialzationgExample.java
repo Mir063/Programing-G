8a6eaabc764a0af0b1b1e052773a888a79edf8a0
Mir063 <buddykabelby@gmail.com>
2025-09-30 22:14:31 +0900

    Add files via upload

---
+++ ch03/sec02/VariableInitialzationgExample.java
@@ -0,0 +1,17 @@
+package ch03.sec02;
+
+public class VariableInitialzationgExample {
+	
+	public static void main(String[] args) {
+		// 변수 value 선언
+		int value = 0;
+		
+		//연산 결과를 변수 result의 초기값으로 대입
+		int result = value;
+		
+		//변수 result 값읅 읽고 콘솔에 입력
+		System.out.println(result);
+	}
+
+
+}
